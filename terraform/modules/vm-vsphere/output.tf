11b406665be4a75180beef74a212078519b1417f
Patrick <privat@patrickjahn.de>
2020-12-07 15:46:49 +0100

    change output

--- terraform/modules/vm-vsphere/output.tf
+++ terraform/modules/vm-vsphere/output.tf
@@ -1,11 +1,11 @@
 output "vm-name" {
  description = "VM-Name"
- value = "${vsphere_virtual_machine.vm-name.name}"
+ value = vsphere_virtual_machine.default.name
 }
 
 
 output "mac-address" {
  description = "Mac-Adress"
- value = "${vsphere_virtual_machine.vm-name.network_interface[0].mac_address}"
+ value = vsphere_virtual_machine.default.network_interface[0].mac_address
 }
 
