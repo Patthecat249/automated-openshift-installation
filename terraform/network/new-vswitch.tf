9efea62415ab1320e978bac9775f307b9b108368
Patthecat249 <privat@patrickjahn.de>
2020-12-14 23:18:53 +0100

    add dvs + dpg

--- terraform/network/new-vswitch.tf
+++ terraform/network/new-vswitch.tf
@@ -7,17 +7,17 @@ provider "vsphere" {
 
 variable "esxi_hosts" {
   default = [
-    "esx01",
-    "esx02"
+    "esx01.home.local",
+    "esx02.home.local"
   ]
 }
 
 variable "network_interfaces" {
   default = [
-    "vmnic0",
-    "vmnic1",
-    "vmnic2",
-    "vmnic3",
+#    "vmnic0",
+#    "vmnic1",
+#    "vmnic2",
+    "vmnic3"
   ]
 }
 
@@ -32,21 +32,33 @@ data "vsphere_host" "host" {
 }
 
 resource "vsphere_distributed_virtual_switch" "dvs" {
-  name          = "terraform-test-dvs"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  name          = "vds-openshift"
+  datacenter_id = data.vsphere_datacenter.dc.id
 
-  #uplinks         = ["uplink1", "uplink2", "uplink3", "uplink4"]
-  #active_uplinks  = ["uplink1", "uplink2"]
-  #standby_uplinks = ["uplink3", "uplink4"]
+#  uplinks         = ["uplink1", "uplink2", "uplink3", "uplink4"]
+#  active_uplinks  = ["uplink3"]
+#  standby_uplinks = ["uplink3", "uplink4"]
 
-  host {
-    host_system_id = "${data.vsphere_host.host.0.id}"
-    devices        = ["${var.network_interfaces}"]
-  }
+#  host {
+#    host_system_id = data.vsphere_host.host.0.id
+#    devices        = ["${var.network_interfaces}"]
+#  }
 
-  host {
-    host_system_id = "${data.vsphere_host.host.1.id}"
-    devices        = ["${var.network_interfaces}"]
-  }
+#  host {
+#    host_system_id = data.vsphere_host.host.1.id
+#    devices        = ["${var.network_interfaces}"]
+#  }
 
+#   pvlan_mapping {
+#   primary_vlan_id = 21
+#   secondary_vlan_id = 22
+#   pvlan_type = "isolated"
+#   }
+}
+
+resource "vsphere_distributed_port_group" "pg" {
+  name                            = "dpg-openshift"
+  distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.dvs.id
+  number_of_ports = 8
+  vlan_id = 20
 }
