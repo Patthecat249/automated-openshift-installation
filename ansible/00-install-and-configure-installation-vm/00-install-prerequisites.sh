cd3b0ea67503a95a2b1cc223934a38a4c885a376
Patrick <privat@patrickjahn.de>
2020-12-15 16:13:47 +0100

    update paths

--- ansible/00-install-and-configure-installation-vm/00-install-prerequisites.sh
+++ ansible/00-install-and-configure-installation-vm/00-install-prerequisites.sh
@@ -12,10 +12,10 @@ unzip terraform_0.14.2_linux_amd64.zip
 cp terraform /usr/local/bin/terraform
 ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa
 sshpass -f rootpassword ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no root@localhost
-ansible all -i localhost, -m user -a "name=pi group=wheel password={{'Test1234' | password_hash('sha512', '')}}"
-#su - pi
-ssh-keygen -t rsa -N '' -f /home/pi/.ssh/id_rsa
-ssh-copy-id -f /home/pi/.ssh/id_rsa pi@pi
-mypath=$(pwd)
+ansible all -i localhost, -m user -a "name=pi group=wheel password={{'Test1234' | password_hash('sha512', '')}} generate_ssh_key=yes ssh_key_bits=2048 ssh_key_file=.ssh/id_rsa"
+mypath=/home/pi
 # Clone Github-Repository and Type Credentials
 mkdir $mypath/git && cd $mypath/git && git config --global credential.helper store && git clone https://github.com/Patthecat249/automated-openshift-installation.git
+chown -R pi:wheel /home/pi/git
+su - pi
+# ssh-copy-id -o StrictHostKeyChecking=no pi@pi
