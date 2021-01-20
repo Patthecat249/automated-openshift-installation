e00ec59922bcbd0779f4057020d319ffca1ee091
Patrick <privat@patrickjahn.de>
2021-01-20 23:13:13 +0100

    install-openshift.sh

---
+++ ansible/04-start-openshift-installation/install-openshift.sh
@@ -0,0 +1,7 @@
+ansible-playbook 01-create-bootstrap.yaml
+ansible-playbook 02-create-master01.yaml
+ansible-playbook 03-create-master02.yaml
+ansible-playbook 04-create-master03.yaml
+ansible-playbook 05-create-worker01.yaml
+ansible-playbook 06-create-worker02.yaml
+ansible-playbook 07-create-worker03.yaml
